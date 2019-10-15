6147ba2230846077da5312c34004a9d7a057a9c3
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-10-15 19:08:32 +0900

    routing_set

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'shifts#index'
+  resources :shifts
 
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
@@ -8,10 +9,8 @@ Rails.application.routes.draw do
 } 
 
   devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
   end
   
 end
