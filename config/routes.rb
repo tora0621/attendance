0db5402c367831b1fec22349efc0e82c6ab0a145
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-10-15 18:52:17 +0900

    signin&up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'shifts#index'
+
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'   
@@ -10,4 +13,5 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+  
 end
