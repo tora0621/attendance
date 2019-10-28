7c10800dcb26a077b18a81661059bd63458ab220
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-10-28 11:31:07 +0900

    sample

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,11 @@ class Shift < ApplicationRecord
 
   belongs_to :worker
   scope :started, -> { where(end_at: nil).order(:start_at) }
+
+  def sum
+    # if start_at.present? && end_at.present?
+      end_at - start_at
+    # end
+  end
+
 end
