cbdf18d909c00e95c813c40d224a81d652cccc76
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-10-30 19:54:11 +0900

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGIX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name,                 presence: true
+  validates :email,                presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGIX }
+  validates :password,             presence: true, length: { minimum: 6, maximum: 20}
 end
