93c61451b9992f29bc15a13d0cfc2499fdb1141e
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-11-02 16:04:13 +0900

    wage_refactoring

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -31,36 +31,32 @@ class Shift < ApplicationRecord
     else
       working_seconds = end_at - start_at - ( rest_end_at - rest_start_at )
     end
-      working_hours = working_seconds / 60 / 60
-      night_border = DateTime.parse "10:00 pm"
+    working_hours = working_seconds / 60 / 60
+    night_border = DateTime.parse "10:00 pm"
 
-      night_border < end_at
-      night_time = (end_at - night_border) / 60 / 60
-      night_wage = (worker.per_hour * 1.25 * night_time).round
-      base_time = (night_border - start_at) / 60 / 60
-      base_wage = (worker.per_hour * base_time).round
-
-      8 >= working_hours
-      base_wage = (working_hours * worker.per_hour).round
-
-      working_hours > 8
-      base_wage = (8 * worker.per_hour).round
-      long_time = working_hours - 8
-      long_wage = (worker.per_hour * 1.25 * long_time).round
-
-      working_hours > 8 && night_border < end_at
+    if working_hours > 8 && night_border < end_at
       base_wage = (8 * worker.per_hour).round
       long_time = working_hours - 8
       long_wage = (worker.per_hour * 1.25 * long_time).round
       night_time = (end_at - night_border) / 60 / 60
       night_wage = (working_hours * 1.5 * night_time)
-      
-      total_wage = base_wage + long_wage + night_wage
-       
-      @shift = Shift.finish.last
-      @wage = Wage.new
-      @wage.attributes = {base: base_wage, long: long_wage, night: night_wage, total: total_wage, worker_id: @shift.worker_id, shift_id: @shift.id}
-      
-      @wage.save
+    elsif night_border < end_at
+      night_time = (end_at - night_border) / 60 / 60
+      night_wage = (worker.per_hour * 1.25 * night_time).round
+      base_time = (night_border - start_at) / 60 / 60
+      base_wage = (worker.per_hour * base_time).round
+    elsif working_hours > 8
+      base_wage = (8 * worker.per_hour).round
+      long_time = working_hours - 8
+      long_wage = (worker.per_hour * 1.25 * long_time).round
+    else 
+      base_wage = (working_hours * worker.per_hour).round  ## 8 >= working_hours
+    end
+
+    total_wage = base_wage + long_wage + night_wage
+    @shift = Shift.finish.last
+    @wage = Wage.new
+    @wage.attributes = {base: base_wage, long: long_wage, night: night_wage, total: total_wage, worker_id: @shift.worker_id, shift_id: @shift.id}
+    @wage.save
   end
 end
