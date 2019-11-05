6bd9829a2502f85c4d70d06045b02432c1656fc1
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-11-05 11:41:06 +0900

    total_wage

--- app/controllers/wages_controller.rb
+++ app/controllers/wages_controller.rb
@@ -1,7 +1,9 @@
 class WagesController < ApplicationController
   def index
-    @wages = Wage.all
+    @wages = Wage.all.includes(:worker)
     # binding.pry
+    d = Wage.all.sum(:total)
+    
   end
   def show
   end
--- app/models/wage.rb
+++ app/models/wage.rb
@@ -1,4 +1,7 @@
 class Wage < ApplicationRecord
   belongs_to :shift
   belongs_to :worker
+
+  # scope :totals, -> { Wage.all.sum(:total) }
+  scope :started, -> { where(end_at: nil).order(:start_at) }
 end
