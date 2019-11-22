c36e8010ac2f84545506938cd307083ca6da3b55
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-11-22 11:21:53 +0900

    comit

--- app/controllers/wages_controller.rb
+++ app/controllers/wages_controller.rb
@@ -8,7 +8,17 @@ class WagesController < ApplicationController
     @wage = Wage.new
   end
   def eat
-    @wage = Worker.new(wage_params)
+    @wage = Wage.new(wage_params)
+    # binding.pry
+    @wage.save
+    render 'makanai'
   end
-
+  private
+  def wage_params
+    # binding.pry
+    params.require(:wage).permit(
+      :worker_id,
+      :meals
+    )
+  end
 end
